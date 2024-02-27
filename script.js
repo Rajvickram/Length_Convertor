c5538d620509e1442cc8e95974c2785c056fe423
Rajvickram <vikrampalani10@gmail.com>
2024-02-27 16:55:10 +0530

    Newly Added JavaScript Code

---
+++ script.js
@@ -0,0 +1,20 @@
+function convert() {
+    let cm = Number(document.getElementById("input").value)
+
+    let inch = cm / 2.54
+
+    let result = document.getElementById("result")
+
+    result.innerHTML = inch.toFixed(2) + "Inch"
+}
+
+
+function call() {
+    let cel = Number(document.getElementById("input2").value)
+
+    let fa = (cel * 9/5) + 32
+
+    let result2 = document.getElementById("result2")
+
+    result2.innerHTML = fa + "Fa"
+}
